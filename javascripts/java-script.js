abd8de5238880f9546dd372b9f197597c680c84a
Соня Ночь <144433433+SofiiaShirenkova@users.noreply.github.com>
2024-06-16 19:41:26 +0300

    Add files via upload

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -5,9 +5,51 @@ document.addEventListener("DOMContentLoaded", function() {
     // Добавляем обработчик события клика
     button_cart[0].addEventListener("click", function() {
     // Изменяем текст кнопки
-    button_cart[0].innerText = "Ты нажал на меня!";
+    button_cart[0].innerText = "В корзине!";
     });
 
-    // document.getElementById('to_cart').innerText = 'nnnn';
+
+
+
+
+
+    
+    // КНОПКА КУПИТЬ -- ВЫЛЕЗАЕТ ФОРМА ДЛЯ ЗАПОЛНЕНИЯ
+    document.addEventListener("DOMContentLoaded", function() {
+        const buy_modal = document.getElementsByClassName("buy_modal");
+        const openModalBtn = document.getElementsByClassName("buy");
+        const closeModalBtn = document.querySelector(".close");
+    
+        // Открыть модальное окно при клике на кнопку
+        openModalBtn[0].addEventListener("click", function() {
+            buy_modal[0].style.display = "block";
+        });
+    
+        // Закрыть модальное окно при клике на крестик
+        closeModalBtn[0].addEventListener("click", function() {
+            buy_modal[0].style.display = "none";
+        });
+    
+        // Закрыть модальное окно при клике вне его области
+        window.addEventListener("click", function(event) {
+        if (event.target == buy_modal[0]) {
+            buy_modal[0].style.display = "none";
+        }
+        });
+    
+        // Отправка формы
+        const form = document.getElementById("buy_myForm");
+        form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Предотвращаем отправку формы
+    
+        // Здесь можно добавить код для отправки данных формы на сервер
+        // alert("Форма отправлена!");
+        buy_modal[0].style.display = "none"; // Закрываем модальное окно после отправки
+        form.reset(); // Сбрасываем содержимое формы
+        });
+        
 
 });
+
+});
+
